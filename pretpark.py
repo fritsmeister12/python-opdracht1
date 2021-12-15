12069124f2fe6fcb400beb6cdc796ee538dedcf8
fritsmeister12 <mattiekill010@gmail.com>
2021-12-15 13:59:00 +0100

    Added in pseudo code and optimized code

--- pretpark.py
+++ pretpark.py
@@ -31,33 +31,38 @@ aantalVolwassenen = int(input('\nOm hoeveel volwassenen gaat het? : '))
 # En tot slot wordt er gevraagd om hoeveel kinderen het gaat
 aantalKinderen = int(input('\nOm hoeveel kinderen gaat het? : '))
 
+print('------------------------------------------------------')
 # Dan wordt er gecheckt of de gebruiker ja heeft geantwoord
 if kaartenOnline[0].lower() == 'j':
     # Dan wordt er gekeken of er een kortingspas aanwezig is
     # Als dit het geval is worden de prijzen aangepast naar 30 en 25 en wordt het totaal berekend
     if metKortingspas[0].lower() == 'j':
         totalePrijs = (aantalVolwassenen * 30) + (aantalKinderen * 25)
-        #
-        print('------------------------------------------------------')
+        # Het totaal bedrag wordt dan laten zien met de tekst dat het online gekocht is met een kortingspas
         print('Online gekocht met kortingspas:', totalePrijs)
     # Als er geen kortingspas aanwezig is worden de prijzen aangepast naar 35 en 28.50 en het totaal berekend
     elif metKortingspas[0].lower() == 'n':
         totalePrijs = (aantalVolwassenen * 35) + (aantalKinderen * 28.50)
-        print('------------------------------------------------------')
+        # Het totaal bedrag wordt dan laten zien met de tekst dat het online gekocht is zonder kortingspas
         print('Online gekocht:', totalePrijs)
     else:
         pass
-    # dsvsddvsdvds
+# Er wordt gecheckt of de gebruiker dan nee heeft geantwoord op online kaarten
 elif kaartenOnline[0].lower() == 'n':
+    # Er wordt gecheckt of de kaarten dan wel een met kortingspas zijn gekocht
     if metKortingspas[0].lower() == 'j':
+        # Als dit het geval is dan wordt de prijs aangepast naar 30 en 25 en wordt het totaal berekend
         totalePrijs = (aantalVolwassenen * 30) + (aantalKinderen * 25)
-        print('------------------------------------------------------')
+        # Het totaal bedrag wordt dan laten zien met daarbij de tekst of het ter plekke gekocht is met een kortingspas
         print('Ter plekke gekocht met kortingspas:', totalePrijs)
+    # Dan wordt er gecheckt of de gebruiker de kaarten niet online gekocht heeft en ook niet met een kortingspas
     elif metKortingspas[0].lower() == 'n':
+        # Als dit het geval is wordt de prijzen aangepast naar 37.50 en 35 en wordt het totaal berekend
         totalePrijs = (aantalVolwassenen * 37.50) + (aantalKinderen * 35)
-        print('------------------------------------------------------')
+        # Het totaal bedrag wordt dan laten zien met daarbij de tekst of het ter plekke gekocht zonder kortingspas
         print('Ter plekke gekocht:', totalePrijs)
     else:
         pass
+# Als er mogelijk iets fout is gegaan wordt er een foutmelding laten zien
 else:
     print('Er gaat iets fout')
